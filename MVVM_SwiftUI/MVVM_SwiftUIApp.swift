2c0d60456f08a3b3d398fe5309b28e70ef4282b7
Kitti Almasy <a.kitty02@gmail.com>
2023-02-01 21:00:16 +1000

    Initial Commit

---
+++ MVVM_SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM_SwiftUI
+//
+//  Created by User on 1/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
